1a7566e186539549bd793391e5628c559cee3e9b
olivaescorial <olivaescorial03@gmail.com>
2022-07-08 02:40:37 +0000

    production tweaks

--- VTVSNsys/settings.py
+++ VTVSNsys/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fj_gn@58@&=2!ukrk-@0j#dmq=hnb^qbylt+da26g*75%f5he%'
-
+#SECRET_KEY = 'django-insecure-fj_gn@58@&=2!ukrk-@0j#dmq=hnb^qbylt+da26g*75%f5he%'
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['votevision.ph']
+ALLOWED_HOSTS = ['votevision.pythonanywhere.com']
 
 
 # Application definition
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'VTVSNsys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'votevision$default',
+        'USER': 'votevision',
+        'PASSWORD': 'escorialdatabase',
+        'HOST': 'votevision.mysql.pythonanywhere-services.com',
     }
 }
 
